1fa1e36ddc1032ff6f0d2d1b8fc796338d1b7bbd
jocelyn <rulinshyu@gmail.com>
2021-12-01 07:05:20 +0800

    feat(checklist): pagination dpne

--- assets/scripts/checklist.js
+++ assets/scripts/checklist.js
@@ -1,15 +1,45 @@
+const docEl = document.documentElement;
 const body = document.querySelector('body');
 const sections = document.querySelectorAll('.section');
 
-window.addEventListener('scroll', () => {
-  let step = '';
-  for (let i = sections.length - 1; i >= 0; i--) {
-    const el = sections[i];
 
-    if (el.getBoundingClientRect().top <= 20) {
-      step = el.dataset.order;
+
+function getTop(elem) {
+  const box = elem.getBoundingClientRect();
+  const scrollTop = window.pageYOffset || docEl.scrollTop || body.scrollTop;
+  const clientTop = docEl.clientTop || body.clientTop || 0;
+  const top  = box.top +  scrollTop - clientTop;
+  return Math.round(top)
+}
+
+let list = [];
+function updateList() {
+  sections.forEach((el, i) => {
+    list[i] = { step: el.dataset.order, top: getTop(el) };
+  });
+}
+
+updateList();
+window.addEventListener('resize', () => {
+  updateList();
+  updateStep();
+});
+
+window.addEventListener('scroll', updateStep);
+
+
+
+function updateStep() {
+  const scrollTop = window.pageYOffset || docEl.scrollTop || body.scrollTop;
+
+  let nextStep = '';
+  for (let i = list.length - 1; i >= 0; i--) {
+    const { step, top } = list[i];
+
+    if (top <= scrollTop + 10) {
+      nextStep = step;
       break;
     }
   }
-  body.dataset.step = step;
-});
+  body.dataset.step = nextStep;
+}
